81e1f5737377a85ddd7e0e40da5516c5dd7d8b9d
igorvlima <65581463+igorvlima@users.noreply.github.com>
2023-10-15 13:33:39 -0300

    Delete alerta-trilho-message-sendler/alerta-trilho-message-sendler directory

--- alerta-trilho-message-sendler/alerta-trilho-message-sendler/src/main/java/com/br/alertatrilhomessagesendler/configuration/SmsSender.java
+++
@@ -1,7 +0,0 @@
-package com.br.alertatrilhomessagesendler.configuration;
-
-import com.br.alertatrilhomessagesendler.domain.SmsRequest;
-
-public interface SmsSender {
-    void sendSms(SmsRequest smsRequest);
-}
